519edf87da06ad6286c70478143abe933e946f28
Niklas Keller <me@kelunik.com>
2023-02-03 22:20:09 +0100

    Update to php-cs-fixer-config v2

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = new Amp\CodeStyle\Config();
+$config = new Amp\CodeStyle\Config;
 $config->getFinder()
     ->in(__DIR__ . '/src')
     ->in(__DIR__ . '/test');
--- test/CertificateTest.php
+++ test/CertificateTest.php
@@ -1,4 +1,5 @@
-<?php /** @noinspection PhpUnhandledExceptionInspection */
+<?php declare(strict_types=1);
+/** @noinspection PhpUnhandledExceptionInspection */
 
 namespace Kelunik\Certificate;
 
